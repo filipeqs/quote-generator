a7538077d9957e13350cb35668443b6c0107a5f7
Filipe Silva <filipeqs@outlook.com>
2021-05-23 13:48:36 -0400

    adding functionality to new quote and twitter button

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    quoteText.textContent = quote.text;
+
+    // Check Quote length to determine styling
+    if (quote.text.length > 100) quoteText.classList.add('long-quote');
+    else quoteText.classList.remove('long-quote');
+
+    authorText.textContent = quote.author || 'Unknown';
 }
 
 // Get Quotes From API
@@ -19,6 +31,16 @@ async function getQuotes() {
         // Catch Error Here
     }
 }
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
